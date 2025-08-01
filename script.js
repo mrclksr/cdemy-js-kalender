1b2d9ffecac38b5748d68b6014828724e5d661eb
Marcel Kaiser <mk@freeshell.de>
2025-08-01 16:36:37 +0200

    Refactor

--- script.js
+++ script.js
@@ -308,31 +308,6 @@ class Month {
     this.init();
   }
 
-  init() {
-    const firstDay = new Date(
-      this.month.getFullYear(),
-      this.month.getMonth(),
-      1
-    );
-    const lastDay = new Date(
-      this.month.getFullYear(),
-      this.month.getMonth() + 1,
-      0
-    );
-    this.daysInMonth = lastDay.getDate();
-    this.firstWeekday = firstDay.getDay();
-    this.lastWeekDay = lastDay.getDay();
-    this.days = [];
-    this.addDays();
-  }
-
-  addDays() {
-    for (let i = 0; i < this.daysInMonth; i++)
-      this.days.push(
-        new Date(this.month.getFullYear(), this.month.getMonth(), i + 1)
-      );
-  }
-
   getYear() {
     return this.month.getFullYear();
   }
@@ -357,6 +332,31 @@ class Month {
   chooseNextMonth() {
     this.month = new Date(this.month.getFullYear(), this.month.getMonth() + 1);
     this.init();
+  }
+  
+  init() {
+    const firstDay = new Date(
+      this.month.getFullYear(),
+      this.month.getMonth(),
+      1
+    );
+    const lastDay = new Date(
+      this.month.getFullYear(),
+      this.month.getMonth() + 1,
+      0
+    );
+    this.daysInMonth = lastDay.getDate();
+    this.firstWeekday = firstDay.getDay();
+    this.lastWeekDay = lastDay.getDay();
+    this.days = [];
+    this.addDays();
+  }
+
+  addDays() {
+    for (let i = 0; i < this.daysInMonth; i++)
+      this.days.push(
+        new Date(this.month.getFullYear(), this.month.getMonth(), i + 1)
+      );
   }
 }
 
